8b8d88409dc939d981978c0f75da5a39e3b82e3f
Houwen Lie <houwenlie98@gmail.com>
2024-12-12 20:08:23 +0700

    Create CurrencyInfoDao and define queries

---
+++ app/src/main/java/com/example/currency_demo/data/local/CurrencyInfoDao.kt
@@ -0,0 +1,38 @@
+package com.example.currency_demo.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.currency_demo.data.model.CurrencyInfo
+import io.reactivex.Completable
+import io.reactivex.Observable
+
+
+/**
+ * @author Houwen Lie (houwenlie98@gmail.com)
+ * @version CurrencyInfoDao, v 0.1 Thu 12/12/2024 8:04 PM by Houwen Lie
+ */
+
+@Dao
+interface CurrencyInfoDao {
+
+    @Query("DELETE FROM currency_info")
+    fun clearAllCurrencies(): Completable
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertCurrency(currencyInfo: CurrencyInfo): Completable
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertCurrencies(currencies: List<CurrencyInfo>): Completable
+
+    @Query(
+        """
+         SELECT * FROM currency_info 
+         WHERE name LIKE :searchQuery || '%' 
+         OR name LIKE '%' || ' ' || :searchQuery || '%' 
+         OR symbol LIKE :searchQuery || '%'
+     """
+    )
+    fun getCurrencies(searchQuery: String): Observable<List<CurrencyInfo>>
+}
