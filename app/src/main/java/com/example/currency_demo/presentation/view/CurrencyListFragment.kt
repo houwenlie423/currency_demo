6267e4e2141a99b4b1d63ce84fc942492fa75ddb
Houwen Lie <houwenlie98@gmail.com>
2024-12-13 21:37:35 +0700

    Show RecyclerView on successful query update

--- app/src/main/java/com/example/currency_demo/presentation/view/CurrencyListFragment.kt
+++ app/src/main/java/com/example/currency_demo/presentation/view/CurrencyListFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.activity.viewModels
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -61,7 +62,10 @@ class CurrencyListFragment : Fragment() {
 
     private fun render(state: CurrencyListState) {
         when (state) {
-            is CurrencyListState.CurrenciesUpdated -> currencyAdapter.submitList(state.currencies)
+            is CurrencyListState.CurrenciesUpdated -> {
+                binding.rvCurrencies.isVisible = true
+                currencyAdapter.submitList(state.currencies)
+            }
 
             is CurrencyListState.Error -> activity?.showToast("SOMETHING BAD")
 
