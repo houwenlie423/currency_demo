5c368eda0279eeaa9d6b7e053446bb52561cd3d0
Houwen Lie <houwenlie98@gmail.com>
2024-12-13 20:06:14 +0700

    Initialize currency list fragment from demo activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/example/currency_demo/presentation/view/DemoActivity.kt
+++ app/src/main/java/com/example/currency_demo/presentation/view/DemoActivity.kt
@@ -2,11 +2,22 @@ package com.example.currency_demo.presentation.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
 import com.example.currency_demo.R
+import com.example.currency_demo.databinding.ActivityDemoBinding
 
 class DemoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDemoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_demo)
+        binding = ActivityDemoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initCurrencyListFragment()
+    }
+
+    private fun initCurrencyListFragment() {
+        supportFragmentManager.commit { replace(R.id.fragment_container, CurrencyListFragment()) }
     }
 }
