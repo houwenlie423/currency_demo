9886c2207699d9a46d7dda41be6124307dce12a5
Houwen Lie <houwenlie98@gmail.com>
2024-12-13 19:11:56 +0700

    Rename main activity into demo activity and move it under presentation package

--- app/src/main/java/com/example/currency_demo/presentation/view/DemoActivity.kt
+++ app/src/main/java/com/example/currency_demo/presentation/view/DemoActivity.kt
@@ -1,11 +1,12 @@
-package com.example.currency_demo
+package com.example.currency_demo.presentation.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.currency_demo.R
 
-class MainActivity : AppCompatActivity() {
+class DemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_demo)
     }
 }
