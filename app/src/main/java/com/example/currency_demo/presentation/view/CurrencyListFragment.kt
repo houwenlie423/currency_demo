bc55aa4598263ebcce553a4d8bcdabdf63edcfd9
Houwen Lie <houwenlie98@gmail.com>
2024-12-13 19:55:56 +0700

    Create layout for RV Currency Items and also CurrencyListFragment

---
+++ app/src/main/java/com/example/currency_demo/presentation/view/CurrencyListFragment.kt
@@ -0,0 +1,11 @@
+package com.example.currency_demo.presentation.view
+
+import androidx.fragment.app.Fragment
+
+
+/**
+ * @author Houwen Lie (houwenlie98@gmail.com)
+ * @version CurrencyListFragment, v 0.1 Fri 12/13/2024 7:44 PM by Houwen Lie
+ */
+class CurrencyListFragment : Fragment() {
+}
