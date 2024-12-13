53fcd0556402e0dab6ce83f1e4730f7b8f025470
Houwen Lie <houwenlie98@gmail.com>
2024-12-13 20:59:43 +0700

    Observe view state test in fragment list

--- app/src/main/java/com/example/currency_demo/presentation/view/CurrencyListFragment.kt
+++ app/src/main/java/com/example/currency_demo/presentation/view/CurrencyListFragment.kt
@@ -5,10 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.currency_demo.databinding.FragmentCurrencyListBinding
 import com.example.currency_demo.presentation.adapter.CurrencyAdapter
+import com.example.currency_demo.presentation.state.CurrencyListState
+import com.example.currency_demo.presentation.utils.showToast
+import com.example.currency_demo.presentation.viewmodel.CurrencyListViewModel
+import com.example.currency_demo.presentation.viewmodel.DemoViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -22,6 +28,8 @@ class CurrencyListFragment : Fragment() {
     private var _binding: FragmentCurrencyListBinding? = null
 
     private val binding get() = _binding!!
+
+    private val viewModel by viewModels<CurrencyListViewModel>()
 
     private val currencyAdapter = CurrencyAdapter()
 
@@ -37,12 +45,27 @@ class CurrencyListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupRecyclerView()
+        observeViewState()
     }
 
     private fun setupRecyclerView() {
         binding.rvCurrencies.apply {
             layoutManager = LinearLayoutManager(requireContext())
             adapter = currencyAdapter
+        }
+    }
+
+    private fun observeViewState() {
+        viewModel.state.observe(viewLifecycleOwner) { state -> render(state) }
+    }
+
+    private fun render(state: CurrencyListState) {
+        when (state) {
+            is CurrencyListState.CurrenciesUpdated -> currencyAdapter.submitList(state.currencies)
+
+            is CurrencyListState.Error -> activity?.showToast("SOMETHING BAD")
+
+            else -> Unit
         }
     }
 
