92762aa05db452ed2f2de8512a8eb13b93803b0c
Houwen Lie <houwenlie98@gmail.com>
2024-12-13 20:22:43 +0700

    Initialize Hilt Application

---
+++ app/src/main/java/com/example/currency_demo/CurrencyApplication.kt
@@ -0,0 +1,12 @@
+package com.example.currency_demo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+/**
+ * @author Houwen Lie (houwenlie98@gmail.com)
+ * @version CurrencyApplication, v 0.1 Fri 12/13/2024 8:21 PM by Houwen Lie
+ */
+@HiltAndroidApp
+class CurrencyApplication : Application()
